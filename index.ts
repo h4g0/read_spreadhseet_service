479c2b2274e1a1768ee0db60e95eadf92d9efa18
hugo-giao <hugo.giao@valeriushub.com>
2021-09-21 15:52:44 +0100

    updates

--- index.ts
+++ index.ts
@@ -12,13 +12,7 @@ const app = express();
 
 const cors = require('cors');
 
-const corsOptions ={
-    origin: "*", 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
-}
-
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use(bodyParser.urlencoded({ extended: false }))
 
